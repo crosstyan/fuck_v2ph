f6c1f24aba08a816269d1c17efaa1fa814159ac0
Crosstyan <cf595833532@gmail.com>
2023-10-08 01:19:09 +0800

    lazy loading is not working

--- src/browser.js
+++ src/browser.js
@@ -1,20 +1,50 @@
+import { random } from "lodash";
 import { handle_images, get_navigation } from "./handle"
 
-(async ()=>{
+(async () => {
   if (document != undefined) {
     /** @type {Set<string>} */
     const uris = new Set();
 
     (async () => {
+      const primary_list = document.getElementsByClassName("photos-list")[0]
+      if (primary_list == undefined) {
+        console.warn("no primary list")
+        return
+      }
       const pages = get_navigation()
       for (let page of pages) {
-
+        const resp = await fetch(page)
+        const text = await resp.text()
+        const parser = new DOMParser()
+        const dom = parser.parseFromString(text, "text/html")
+        const scripts = dom.querySelectorAll("script")
+        const photo_lists = dom.querySelectorAll(".photos-list")[0]
+        if (photo_lists == undefined) {
+          console.warn(`no photos-list in ${page.toString()}`)
+          continue
+        }
+        primary_list.insertAdjacentElement("afterend", photo_lists)
+        const sleep_ms = random(300, 1000)
+        console.log(`sleeping ${sleep_ms}ms`)
+        for (let s of scripts) {
+          if (s.textContent.length == 0) {
+            continue
+          }
+          console.log(`evaluating ${s.textContent.length} bytes in ${page.toString()}`)
+          eval(s.textContent)
+        }
+        await new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve()
+          }, sleep_ms)
+        })
       }
-      // const lists = document.getElementsByClassName("photos-list")
-      // for (let list of lists) {
-      //   const imgs = list.querySelectorAll("img")
-      //   await handle_images(imgs, uris)
-      // }
+      const lists = document.getElementsByClassName("photos-list")
+      for (let list of lists) {
+        const imgs = list.querySelectorAll("img")
+        await handle_images(imgs, uris)
+      }
     })()
   }
 })()
